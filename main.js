0afa543b2d85f0084c59777cd04484b355482222
Mykhailo Halaida <galaida.m@gmail.com>
2020-01-28 10:58:34 +0200

    Finalized inverted index calculation

--- main.js
+++ main.js
@@ -1,44 +1,63 @@
 let fs = require("fs");
-let outputDict = "dictFinal.txt";
+let outputIndex = "indexFinal.txt";
 let inputDir = "./inputcollection/";
 let inDirFileArr = [];
-let totalWordsCounter = 0;
-let wordsInDictFin = 0;
 let t0 = new Date()
+var auxData = [];
+var auxIndex = {};
+var invIndex = {};
 
 //GETTING THE FILENAMES
 fs.readdirSync(inputDir).forEach(file => {
     inDirFileArr.push(file);
 });
 
-//CREATING ARRAY TO STORE ALL WORDS WITH THEIR OCCURENCES
-let invIndex = {};
-
+//AUXDATA ARRAY FOR WORDS ANS AUXINDEX OBJECT FOR THEIR OCCURENCES
 inDirFileArr.forEach(function (fileName, fileIndex) {
     let filepath = inputDir + fileName;
     let data = fs.readFileSync(filepath).toString('utf-8');
-    data = data.toUpperCase().split(/[\W\d\s_]/);
+    data = data.toUpperCase().split(/[^a-zA-Z]/);
+    auxData = auxData.concat(data);
     data.forEach(word => {
-        if (invIndex[word] == undefined) {
-            invIndex[word] = [];
-        } 
-        invIndex[word].push(fileIndex + 1);
+        if (isNaN(word)) {
+            if (auxIndex[word] == undefined) {
+                auxIndex[word] = [];
+            }
+            auxIndex[word].push(fileIndex + 1);
+        }
     });
 })
 
+//REMOVING DUPLICATES FROM AUXINDEX VALUES
+for (const unsWord in auxIndex) {
+    let auxWordSet = new Set(auxIndex[unsWord]);
+    auxIndex[unsWord] = [...auxWordSet];
+}
+
+//MOVING THE OCCURENCES TO INVINDEX OBJECT
+auxData.sort();
+auxData.forEach(unsWord => {
+    if (isNaN(unsWord)) {
+        if (invIndex[unsWord] == undefined) {
+            invIndex[unsWord] = [];
+        }
+        invIndex[unsWord].push(auxIndex[unsWord]);
+    }
+});
+
+//REMOVING DUPLICATES FROM INVINDEX VALUES
 for (const word in invIndex) {
     let auxSet = new Set(invIndex[word]);
     invIndex[word] = [...auxSet];
 }
 
-fs.writeFile(outputDict, '', function () { });
-let writer = fs.createWriteStream(outputDict, { flags: 'a' });
+let t1 = new Date();
+
+//WRITING TO FILE
+fs.writeFile(outputIndex, '', function () { });
+let writer = fs.createWriteStream(outputIndex, { flags: 'a' });
 for (const word in invIndex) {
     writer.write(word + " => " + invIndex[word] + "\n");
 }
 
-let t1 = new Date();
-
-console.log("\nTotal number of words in collection: " + totalWordsCounter);
-console.log("Total number of words in dictionary: " + wordsInDictFin);
-console.log("Execution time: " + (t1-t0) + "ms\n");
+console.log("Execution time: " + (t1 - t0) + "ms\n");
