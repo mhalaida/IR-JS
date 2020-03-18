966fa14659b59bf912c03ee13d0d78ef217ca162
Mykhailo Halaida <galaida.m@gmail.com>
2020-03-18 22:18:02 +0200

    [WIP] Basic zone index without frequency

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-let spimi = require("./spimi");
-let outputDir = "/Users/m.halaida/Desktop/SPIMI_SMALL/"
-let inputDir = "/Users/m.halaida/Desktop/gutenberg/4/0/0";
+let fs = require("fs");
 
-// fileStream = [];
-// let result = spimi.fileStream(inputDir, fileStream);
-// console.table(result)
-// console.log("Finished building the filestream");
-// console.log(spimi.buildSpimi(result, outputDir));
+let zi = require("./zoneindex");
 
-// console.log(spimi.mergeBlocks(outputDir));
+let zoneIndex = zi.buildZoneIndex("./inputcollection");
 
-let masterInput = "/Users/m.halaida/Desktop/SPIMI_SMALL/master";
-let compIndex = "/Users/m.halaida/Desktop/SPIMI_SMALL/compindex";
-let compDict = "/Users/m.halaida/Desktop/SPIMI_SMALL/compdict";
-spimi.compressIndex(masterInput, compIndex, compDict)
+fs.writeFile("TEST_OUTPUT.txt", '', function () { });
+let writerInd = fs.createWriteStream("TEST_OUTPUT.txt", { flags: 'a' });
+for (const word in zoneIndex) {
+    writerInd.write(word + " => " + [...zoneIndex[word]] + "\n");
+}
